63a33f030d0ccb607d8595777405dbb373215f70
AlexG0311 <alexdavidgonzalez2003@gmail.com>
2024-10-17 23:30:19 -0500

    primer commit

---
+++ Rultimo/Rultimo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Rultimo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
