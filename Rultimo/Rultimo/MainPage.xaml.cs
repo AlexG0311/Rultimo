a304ff9ff084326caa2721b1bd645e978f8bf618
AlexG0311 <alexdavidgonzalez2003@gmail.com>
2024-10-18 00:13:24 -0500

    corchu

--- Rultimo/Rultimo/MainPage.xaml.cs
+++ Rultimo/Rultimo/MainPage.xaml.cs
@@ -3,7 +3,7 @@
     public partial class MainPage : ContentPage
     {
         int contador = 0;
-      
+
 
         public MainPage()
         {
@@ -13,15 +13,20 @@
         private void OnButtonClicked(object sender, EventArgs e)
         {
             contador++; // Incrementar el contador
-            lblContador.Text = $"Has presionado el botón {contador} veces"; // Actualizar el texto del Label
-        private void OnNewButtonClicked(object sender, EventArgs e)
-        {
-            DisplayAlert("New Button", "This button was added by a collaborator!", "OK");
-        private void OnChangeColorButtonClicked(object sender, EventArgs e)
-        {
-            this.BackgroundColor = Colors.LightBlue;
-        }
+            lblContador.Text = $"Has presionado el botón {contador} veces";
+        }// Actualizar el texto del Label
+            private void OnNewButtonClicked(object sender, EventArgs e)
+                
+            {
+                DisplayAlert("New Button", "This button was added by a collaborator!", "OK");
+            }
+                private void OnChangeColorButtonClicked(object sender, EventArgs e)
+                {
+                    this.BackgroundColor = Colors.LightBlue;
+                }
 
-    }
+     }
 
 }
+    
+
